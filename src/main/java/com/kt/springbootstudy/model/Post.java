285c4066a2605b4c8cfd11aecd9ce9523ddcfe34
yoonha <yoonha0124@naver.com>
2025-02-20 08:40:57 +0900

    feat: add Post Model

---
+++ src/main/java/com/kt/springbootstudy/model/Post.java
@@ -0,0 +1,51 @@
+package com.kt.springbootstudy.model;
+
+public class Post {
+    private int id; // int 타입 ID
+    private String title; // 글 제목
+    private String content; // 글 내용
+    private String author; // 작성자
+
+    public Post() {
+    }
+
+    public Post(int id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
