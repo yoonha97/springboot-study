1a0ec6aabc613b75f1b265fd4c27519489f0ab16
yoonha <yoonha0124@naver.com>
2025-02-20 08:47:16 +0900

    fix: update return message in HelloController

--- src/main/java/com/kt/springbootstudy/controller/HelloController.java
+++ src/main/java/com/kt/springbootstudy/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello World!";
+		return "Hello! This is My Simple CRUD Service!";
 	}
 
 }
