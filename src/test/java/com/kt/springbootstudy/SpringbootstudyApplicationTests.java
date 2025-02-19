c2d662bc21177fe595c00f8df56dd3c39fcbd7da
yoonha <yoonha0124@naver.com>
2025-02-19 18:11:51 +0900

    initial commit

---
+++ src/test/java/com/kt/springbootstudy/SpringbootstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
