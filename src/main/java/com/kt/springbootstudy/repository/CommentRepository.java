d0019d31fe948a780584b7a0a49523ee83eeb18a
yoonha <yoonha0124@naver.com>
2025-02-26 09:58:46 +0900

    feat: add CommentRepository

---
+++ src/main/java/com/kt/springbootstudy/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.kt.springbootstudy.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kt.springbootstudy.model.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+	List<Comment> findByPostId(int postId);
+}
